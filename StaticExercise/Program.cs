3419396f5421fd372ff270b0aec953730987b53a
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-02-10 14:15:17 -0600

    added static method

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var c = TempConverter.FahrenheitToCelsius(77);
+            var f = TempConverter.CelsiusToFahrenheit(27);
+
+            Console.WriteLine($"Celsius: {c}");
+            Console.WriteLine($"Fahrenheit: {f}");
         }
     }
 }
